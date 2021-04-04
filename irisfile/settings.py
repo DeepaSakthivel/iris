a7321d2560a9afb490ce152e501508ad9294f9f9
DeepaSakthivel <deepasakthivel115@gmail.com>
2021-04-04 15:55:00 +0530

    edit

--- irisfile/settings.py
+++ irisfile/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!dr@!yk54)a=958i(io36yp(4$()h7v)1p3rio2d*m=9__oafj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['18.222.158.201','localhost']
+ALLOWED_HOSTS = ['3.22.216.165','localhost']
 
 
 # Application definition
